8cfaefdc800fa2e9f20c777acb4a8d904e72774e
Chen Luo <luochen@umich.edu>
2015-03-17 20:45:36 -0400

    fix construct

--- AzureUpload/AzureUpload/Program.cs
+++ AzureUpload/AzureUpload/Program.cs
@@ -7,121 +7,68 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Threading;
 
 namespace AzureUpload
 {
     class Program
     {
-        static string connString;
-        static string downFolder;
-        static string upFolder;
-        static string upContainter;
-        static string downContainer;
-
-        static DateTime prevTime;
-        static CloudStorageAccount storage_account;
-        static CloudBlobClient blob_client;
-        static CloudBlobContainer container_down;
-        static CloudBlobContainer container_up;
-
         static void Main(string[] args)
         {
-            connString = ConfigurationManager.ConnectionStrings["AzureStorageAccount"].ConnectionString;
-            downFolder = ConfigurationManager.AppSettings["downFolder"];
-            upFolder = ConfigurationManager.AppSettings["upFolder"];
-            upContainter = ConfigurationManager.AppSettings["destContainer"];
-            downContainer = ConfigurationManager.AppSettings["localContainer"];
-            DateTime prevTime = new DateTime(2015, 3, 13); // Timestamp to start analyze
+            string connString = ConfigurationManager.ConnectionStrings["AzureStorageAccount"].ConnectionString;
+            string localFolder = ConfigurationManager.AppSettings["sourceFolder"];
+            string destContainter = ConfigurationManager.AppSettings["destContainer"];
 
+            Console.Write(@"connect to the account");
+            CloudStorageAccount sa = CloudStorageAccount.Parse(connString);
+            CloudBlobClient bc = sa.CreateCloudBlobClient();
 
-            Console.Write("connect to the account \n");
-            storage_account = CloudStorageAccount.Parse(connString);
-            blob_client = storage_account.CreateCloudBlobClient();
+            Console.Write(@"get reference to con");
+            CloudBlobContainer container = bc.GetContainerReference(destContainter);
 
-            Console.Write("get reference to container \n");
-            container_down = blob_client.GetContainerReference(downContainer);
-            container_up = blob_client.GetContainerReference(upContainter);
+            container.CreateIfNotExists();
 
-
-            container_down.CreateIfNotExists();
-            container_up.CreateIfNotExists();
-
-            System.Threading.Thread downloadThread = new System.Threading.Thread(downloadFile);
-            System.Threading.Thread uploadThread = new System.Threading.Thread(uploadFile);
-            downloadThread.Start();
-            uploadThread.Start();
-        }
-
-        static void downloadFile()
-        {
-            while (true)
+            foreach (var blockblob in container.ListBlobs())
             {
-                System.Threading.Thread.Sleep(5000);
-                foreach (var blockblob in container_down.ListBlobs())
-                {
-                    CloudBlockBlob b = (CloudBlockBlob)blockblob;
-                    string path = downFolder + "\\" + b.Name;
-                    DownloadBlob(b, path, prevTime);
-                }
-                prevTime = DateTime.Now;
-            }
-        }
-
-        static void uploadFile()
-        {
-            while (true)
-            {
-                Thread.Sleep(5000);
-                string[] fileEntries = Directory.GetFiles(upFolder);
-                foreach (string filePath in fileEntries)
-                {
-                    //string key = DateTime.UtcNow.ToString("yyyy-MM-dd-HH") + "-" + Path.GetFileName(filePath);
-                    UploadBlob(container_up, filePath, Path.GetFileName(filePath), prevTime);
-
-                }
-                prevTime = DateTime.Now;
-            }
-        }
-
-        static void UploadBlob(CloudBlobContainer container, string filePath, string filename, DateTime dt)
-        {
-            CloudBlockBlob b = container.GetBlockBlobReference(filename);
-
-            using (var fs = System.IO.File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.None))
-            {
-                BlobRequestOptions options = new BlobRequestOptions();
-                AccessCondition ac = AccessCondition.GenerateIfModifiedSinceCondition(dt.ToUniversalTime());
-
-                try
-                {
-                    b.UploadFromStream(fs, ac, null, null);
-                    Console.WriteLine(@"uploading file to container: key =" + filename);
-                }
-                catch (StorageException)
-                {
-                    Console.Write("no file to upload\n");
-                }
+                CloudBlockBlob b = (CloudBlockBlob) blockblob;
+                string path = localFolder + "\\" + b.Name;
+                DownloadBlob(b, path);
             }
 
+            /*string[] fileEntries = Directory.GetFiles(localFolder);
+            foreach (string filePath in fileEntries)
+            {
+                string key = DateTime.UtcNow.ToString("yyyy-MM-dd-HH:mm:ss") + "-" + Path.GetFileName(filePath);
+                UploadBlob(container, key, filePath, false);
+            }
+
+            Console.WriteLine(@"upload processing complete");
+            Console.ReadKey();*/
+
         }
 
-        static void DownloadBlob(CloudBlockBlob b, string path, DateTime currTime)
+        static void UploadBlob(CloudBlobContainer container, string key, string filename, bool deleteAfter)
         {
-            
-            BlobRequestOptions option = new BlobRequestOptions();
+
+            Console.WriteLine(@"uploading file to container: key =" + key + " sourcefile= " + filename);
+            CloudBlockBlob b = container.GetBlockBlobReference(key);
+
+            using (var fs = System.IO.File.Open(filename, FileMode.Open, FileAccess.Read, FileShare.None))
+            {
+                b.UploadFromStream(fs);
+            }
+
+            if (deleteAfter)
+                File.Delete(filename);
+
+        }
+
+        static void DownloadBlob(CloudBlockBlob b, string path)
+        {
+            Console.Write(path);
             using (var fileStream = System.IO.File.OpenWrite(path))
             {
-                try
-                {
-                    b.DownloadToStream(fileStream, AccessCondition.GenerateIfModifiedSinceCondition(currTime.ToLocalTime()));
-                    Console.Write("Download to " + path + "\n");
-                }
-                catch(Microsoft.WindowsAzure.Storage.StorageException)
-                {
-                    Console.Write("No new file to download \n");
-                }
-            }
+                b.DownloadToStream(fileStream);
+            } 
         }
     }
 }
